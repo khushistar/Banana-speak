72ea75c71725f90e1345baf05ecf0988eb2f4ec8
khusistar <khushikunwar201595@gmail.com>
2023-11-28 20:59:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ var textInput = document.querySelector("#text-input");
 var button = document.querySelector("#translate-btn");
 var textOutput = document.querySelector("#output");
 
-var serverAPI = "https://api.funtranslations.com/translate/minion.json"
+var serverAPI = "https://api.funtranslations.com/translate/minion.json";
 
 function geturl(input) {
-    return  serverAPI + "?" + "text=" + input
+  return serverAPI + "?" + "text=" + input;
 }
-function errorhandle(error){
-    console.log(error)
-    alert("Something  went wrong with server. Please try again")
+function errorhandle(error) {
+  console.log(error);
+  alert("Something  went wrong with server. Please try again");
 }
 function clickHandle() {
-    var inputvalue = textInput.value;
-    fetch(geturl(inputvalue ))
-        .then((res) => res.json())
-        .then((result) => {
-            console.log(result);
-            textOutput.innerText = result.contents.translated;
-        }).catch(errorhandle)
+  var inputvalue = textInput.value;
+  fetch(geturl(inputvalue))
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      textOutput.innerText = result.contents.translated;
+    })
+    .catch(errorhandle);
 }
 
 button.addEventListener("click", clickHandle);
